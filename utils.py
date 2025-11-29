79407f57aa955990dc35ab871f8ab3938ca446ba
man00154 <149796066+man00154@users.noreply.github.com>
2025-11-29 13:55:07 +0530

    Refactor load_json and save_json to use file names

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 import json
+import os
 
-def load_json(path):
+BASE = os.path.dirname(os.path.abspath(__file__))
+
+def load_json(file):
+    path = os.path.join(BASE, "config", file)
     with open(path) as f:
         return json.load(f)
 
-def save_json(path, content):
+def save_json(file, content):
+    path = os.path.join(BASE, "config", file)
     with open(path, "w") as f:
         json.dump(content, f, indent=4)
